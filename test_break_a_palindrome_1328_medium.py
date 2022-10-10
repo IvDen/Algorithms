0f26a8ee3ce0c9d06d83d2a3ed94298f60e04779
Denis Ivanov <ivanov.d.v2@gmail.com>
2022-10-10 13:39:05 +0300

    task 1328 done

---
+++ test_break_a_palindrome_1328_medium.py
@@ -0,0 +1,27 @@
+import unittest
+
+from break_a_palindrome_1328_medium import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solution = Solution()
+
+    def test_base0(self):
+        self.assertEqual(self.solution.breakPalindrome('a'), '')
+
+    def test_base1(self):
+        self.assertEqual(self.solution.breakPalindrome('abccba'), 'aaccba')
+
+    def test_base2(self):
+        self.assertEqual(self.solution.breakPalindrome('aa'), 'ab')
+
+    def test_base3(self):
+        self.assertEqual(self.solution.breakPalindrome('aba'), 'abb')
+
+    def test_base_empty_input(self):
+        self.assertEqual(self.solution.breakPalindrome(''), '')
+
+
+if __name__ == "__main__":
+    unittest.main()
